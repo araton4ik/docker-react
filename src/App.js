33ac5436662b2c40498694f06a4c3112be8316a6
aliaksandr_yazerski <aezerski1@gmail.com>
2022-11-30 15:39:41 +0400

    changed message

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello from AWS!</p>
+        <p>Hello from Github!</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
